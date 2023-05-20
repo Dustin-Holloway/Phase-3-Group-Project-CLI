05a748d721192296d5a560f9c0175b26dadd7b31
Dustin <dustinhollowaypdx@gmail.com>
2023-05-20 15:15:08 -0700

    all done

--- lib/cli.py
+++ lib/cli.py
@@ -22,12 +22,13 @@ ok_bye = '''
 '''
 
 returning='''
-████████╗██╗░░██╗░█████╗░███╗░░██╗██╗░░██╗░██████╗
-╚══██╔══╝██║░░██║██╔══██╗████╗░██║██║░██╔╝██╔════╝
-░░░██║░░░███████║███████║██╔██╗██║█████═╝░╚█████╗░
-░░░██║░░░██╔══██║██╔══██║██║╚████║██╔═██╗░░╚═══██╗
-░░░██║░░░██║░░██║██║░░██║██║░╚███║██║░╚██╗██████╔╝
-░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝╚═════╝░
+
+██╗░░░██╗░█████╗░██╗░░░██╗  ██████╗░███████╗████████╗██╗░░░██╗██████╗░███╗░░██╗███████╗██████╗░
+╚██╗░██╔╝██╔══██╗██║░░░██║  ██╔══██╗██╔════╝╚══██╔══╝██║░░░██║██╔══██╗████╗░██║██╔════╝██╔══██╗
+░╚████╔╝░██║░░██║██║░░░██║  ██████╔╝█████╗░░░░░██║░░░██║░░░██║██████╔╝██╔██╗██║█████╗░░██║░░██║
+░░╚██╔╝░░██║░░██║██║░░░██║  ██╔══██╗██╔══╝░░░░░██║░░░██║░░░██║██╔══██╗██║╚████║██╔══╝░░██║░░██║
+░░░██║░░░╚█████╔╝╚██████╔╝  ██║░░██║███████╗░░░██║░░░╚██████╔╝██║░░██║██║░╚███║███████╗██████╔╝
+░░░╚═╝░░░░╚════╝░░╚═════╝░  ╚═╝░░╚═╝╚══════╝░░░╚═╝░░░░╚═════╝░╚═╝░░╚═╝╚═╝░░╚══╝╚══════╝╚═════╝░
 '''
 
 noble_steed='''
@@ -119,7 +120,7 @@ def welcome():
         ))
     click.echo(
         click.style(
-            "\n\n" + "Welcome to the Sracket CLI!", 
+            "\n\n" + "                              Welcome to the Sracket CLI!", 
             blink=True,
             bold=True,
             fg="green",
@@ -130,7 +131,7 @@ def welcome():
 
 
 @click.command()
-@click.option("--name", prompt=click.style("\n\n\n" + "Please enter your name to begin", blink=True,
+@click.option("--name", prompt=click.style("\n\n\n" + "                         Please enter your name to begin", blink=True,
             bold=True,
             fg="green",
 ))
@@ -165,9 +166,9 @@ def select_option1():
     click.echo(click.style("\n" + f"                             {current_user.name}!" + "\n\n", bold=True, fg="red",))
 
     options = ["Rent", "Exit"]
-    prompt = "\n\n\n" + "Would you like to rent a " + click.style("siq bike? ", blink=True,
+    prompt = click.style("\n\n\n             Would you like to rent a siq bike?"+"\n\n\n" + "                   ", blink=True,
             bold=True,
-            fg="green",) + "\n\n"
+            fg="green")
     selected_option = click.prompt(
         prompt, type=click.Choice(options, case_sensitive=False)
     )
@@ -184,8 +185,8 @@ def handle_option1(option):
         session.delete(user)
         session.commit()
         click.clear()
-        click.echo(ok_bye)
-        click.echo("\n\n\n" + "See you later!" +"\n\n\n\n")
+        click.echo(click.style((ok_bye),fg="green"))
+        # click.echo("\n\n\n" + "See you later!" +"\n\n\n\n")
         
 #################################### If does exist and has bike - option 2 ########################################
         
@@ -196,7 +197,7 @@ def select_option2():
     click.echo(click.style("\n" + (welcome_back), bold=True, fg="green"))
     click.echo(click.style("\n" + f"                             {current_user.name}!" + "\n\n", bold=True, fg="red",))
     options = ["I'm done", "More shred"]
-    prompt = "\n" + "Are you finished with your ride?, or did you still want to shred???" + "\n\n\n"
+    prompt = "\n" + "Are you finished with your ride?, or did you still want to shred???" + "\n\n\n" + "                    "
     selected_option = click.prompt(
         prompt, type=click.Choice(options, case_sensitive=False)
     )
@@ -213,8 +214,8 @@ def handle_option2(option):
         click.clear()
         click.echo("\n\n\n\n"+ "        We'll see you when you get back!")
 
-        click.echo("\n\n\n" +   wheels_up)
-        click.echo("\n" + "             Keep it on two wheels!"+ "\n\n\n\n\n\n\n\n")
+        click.echo(click.style("\n\n\n" +   wheels_up, fg="green"))
+        click.echo(click.style("\n" + "             Keep it on two wheels!"+ "\n\n\n", fg="green"))
 
 
 #################################### If user does exist - option 3 ########################################
@@ -224,8 +225,8 @@ def select_option3():
     click.clear()
     click.echo(click.style("\n" + (welcome_back), bold=True, fg="green"))
     click.echo(click.style("\n" + f"                             {current_user.name}!" + "\n\n", bold=True, fg="red",))
-    options = ["Yes", "Exit"]
-    prompt = "\n\n" + "Would you like to rent a bike today?" + "\n\n\n"
+    options =["Yes", "Exit"]
+    prompt = click.style("\n\n" + "               Would you like to rent a bike today?" + "\n\n\n" + '                       ', fg="green", bold="true")
     selected_option = click.prompt(
         prompt, type=click.Choice(options, case_sensitive=False)
     )
@@ -238,8 +239,8 @@ def handle_option3(option):
         enter_locations()
     elif option == "Exit":
         click.clear()
-        click.echo(ok_bye)
-        click.echo("\n\n\n" + "See you later!" +"\n\n\n\n")
+        click.echo(click.style(ok_bye,fg="green", blink=True))
+        # click.echo("\n\n\n" + "See you later!" +"\n\n\n\n")
 
 ################## Display locker_locations, Displays Bike table, Handles selections ######################
 
@@ -263,43 +264,41 @@ def select_bike(bike_id):
     user = session.query(User).filter(User.name == current_user.name).first()
     headers = [selected_bike.id]
     table = tabulate([[selected_bike.name]], headers, tablefmt="mixed_grid")
+      # Begin datatime
+
+    start_time = time.ctime()
+    datetime_obj = datetime.strptime(start_time, "%a %b %d %H:%M:%S %Y")
+    user.start_time = datetime.now()
+    session.commit()
     click.clear()
-    click.echo(noble_steed)
-    click.echo(click.style((table), bg="bright_green", fg="black", bold=True) + "\n\n")
+    click.echo(click.style(noble_steed, fg="green"))
+    click.echo(click.style((table), bg="bright_green", fg="black", bold=True) + click.style(f"  Your START TIME is: {user.start_time}" + "\n\n\n\n" + "\n\n", fg="green"))
     selected_bike.locker_id = None
     session.commit()
     user.bike = selected_bike.id
     session.commit()
     selected_bike.user_id = user.id
     session.commit()
-    user.start_time = datetime.now()
-    session.commit()
-
-    # Begin datatime
-
-    start_time = time.ctime()
-    datetime_obj = datetime.strptime(start_time, "%a %b %d %H:%M:%S %Y")
-    click.echo(f"Start: {user.start_time}" + "\n\n\n\n")
-    click.echo("           Keep it on two wheels!!!" + "\n\n\n\n\n\n")
-    click.echo(wheels_up + "\n\n\n\n\n")
+    click.echo(click.style("           Keep it on two wheels!!!" + "\n\n\n", fg="green"))
+    click.echo(click.style(wheels_up + "\n\n\n\n\n", fg="green"))
 
 
 @click.command()
 @click.option(
-    "--location_id", prompt="\n\n\n\n\n" + "Which location are you renting from today?  Please enter the hub ID", type=int)
+    "--location_id", prompt="\n\n\n" + "Which location are you renting from today?  Please enter the hub ID", type=int)
 def enter_locations(location_id):
     lockers = session.query(Locker).all()
     headers = [l.id for l in lockers]
     locker = [l.locker_location for l in lockers]
     table = tabulate([locker], headers, tablefmt="mixed_grid")
-    click.echo(click.style("\n\n" + (table), bg="bright_green", fg="black", bold=True))
+    click.echo(click.style("\n\n\n" + "         " + (table), bg="bright_green", fg="black", bold=True))
     locker = session.query(Locker).filter(Locker.id == location_id).first()
     click.clear()
     display_table(locker)
 
 
 def display_table(locker):
-    click.echo(bike_locker)
+    click.echo(click.style(bike_locker, fg="green"))
     bikes = [bike.name for bike in locker.bikes if bike.locker_id]
     headers = [b.id for b in locker.bikes]
     table = tabulate([bikes], headers, tablefmt="mixed_grid")
@@ -321,7 +320,7 @@ def return_bike():
 
 @click.command()
 @click.option(
-    "--location_id", prompt="\n\n\n" + "Which location are returning your bike to?  Please enter the hub ID", type=int
+    "--location_id", prompt=click.style("\n\n\n" + "Which location are returning your bike to?  Please enter the hub ID",fg="green"), type=int
 )
 def return_locations(location_id):
     click.clear()
@@ -340,8 +339,17 @@ def display_bike_to_return(locker):
     user = session.query(User).filter(User.name == current_user.name).first()
     headers = [returned_bike.id]
     table = tabulate([[returned_bike.name]], headers, tablefmt="mixed_grid")
-    click.echo(returning + "\n\n")
-    click.echo(click.style((table), bg="bright_green", fg="black", bold=True) + "\n\n")
+    click.echo(click.style(returning + "\n",fg="green"))
+    endtime = time.ctime()
+    datetime_obj = datetime.strptime(endtime,"%a %b %d %H:%M:%S %Y" )
+    current_user.end_time = datetime_obj
+    session.commit()
+    # click.echo(f"Start: {current_user.start_time} - End: {current_user.end_time}" + "\n\n" )
+    start_datetime = current_user.start_time
+    end_datetime = current_user.end_time
+    elapsed_timedelta = end_datetime - start_datetime
+    elapsed_minutes = elapsed_timedelta.total_seconds() / 60
+    click.echo(click.style((table), bg="bright_green", fg="black", bold=True) + f"    YOU STARTED YOUR RIDE AT: {current_user.start_time} - YOU ENDED YOUR RIDE AT: {current_user.end_time}" + "\n\n")
     returned_bike.locker_id = locker.id
     returned_bike.user_id = None
     user.bike = None
@@ -350,17 +358,10 @@ def display_bike_to_return(locker):
     # Handling datetime
 
 
-    endtime = time.ctime()
-    datetime_obj = datetime.strptime(endtime,"%a %b %d %H:%M:%S %Y" )
-    current_user.end_time = datetime_obj
-    session.commit()
-    click.echo(f"Start: {current_user.start_time} - End: {current_user.end_time}" + "\n\n" )
-    start_datetime = current_user.start_time
-    end_datetime = current_user.end_time
-    elapsed_timedelta = end_datetime - start_datetime
-    elapsed_minutes = elapsed_timedelta.total_seconds() / 60
-    total = round(float(elapsed_minutes * 5.00), 2)
-    click.echo(f"Your total is ${total}" + "\n\n\n")
+    
+    total = round(float(elapsed_minutes * 3.00), 2)
+    minutes = round(float(elapsed_minutes), 3)
+    click.echo(f"YOUR TOTAL WILL BE ${total} + FOR  {minutes} MINS OF SHREDDING!" + "\n\n\n")
     user.start_time = None
     session.commit()
     user.end_time = None
